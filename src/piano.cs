40b110e3602c599660a4f5053425a23d1de31305
prima <primarina314@snu.ac.kr>
2024-10-01 16:56:56 +0000

    create piano tiles

--- src/piano.cs
+++ src/piano.cs
@@ -7,11 +7,43 @@ namespace PianoTiles
 {
 	class Sheet
 	{
+		private List<SoundUnit> sounds;
+		public<SoundUnit> Sounds { get { return sounds; } }
 		
+		public Sheet(string src = "")
+		{
+			sounds = new List<SoundUnit>();
+		}
+		
+		public void Load(string src)
+		{
+			
+		}
 	}
 	
 	enum Rhythm
 	{
-		fullNote, halfNote, quarterNote
+		fullNote, halfNote, quarterNote, eighthNote, sixteenthNote, thirtysecondNote,
+		dottedHalfNote, dottedQuarterNote, dottedEighthNote, dottedSixteenthNote, dottedThirtysecondNote,
+		fullRest, halfRest, quarterRest, eighthRest, sixteenthRest, thirtysecondRest,
+		dottedHalfRest, dottedQuarterRest, dottedEighthRest, dottedSixteenthRest, dottedThirtysecondRest
+	}
+	
+	enum PitchName { C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B }
+	
+	struct Pitch
+	{
+		public Pitch(PitchName pitchname = PitchName.A, int octave = 0)
+		{
+			this.pitchname = pitchname;
+			this.octave = octave;
+		}
+		public PitchName pitchname;
+		public int octave;
+	}
+	
+	struct SountUnit
+	{
+		
 	}
 }
