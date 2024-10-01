c43ef809634e9aa57aa52f922ee7768ef4a08d8b
prima <primarina314@snu.ac.kr>
2024-10-01 16:29:56 +0000

    adjust cele/utils

--- src/main.cs
+++ src/main.cs
@@ -8,11 +8,18 @@ using System.Text;
 using System.Runtime.InteropServices;
 using System.Drawing;
 using SnakeGame;
+using Celestial;
 
 public class main {
 	public static void Main() {
-		GameManager gm = new GameManager();
-		gm.GameStart();
+		Simulation simul = new Simulation(0.001M);
+		
+		simul.AddObject(new CelestialObject(0M, 0M, 0M, 0M, 1000M, 1M));
+		simul.AddObject(new CelestialObject(10M, 0M, 0M, 10M, 10M, 1M));
+		
+		// public CelestialObject(Decimal posX=0M, Decimal posY=0M, Decimal veloX=0M, Decimal veloY=0M, Decimal mass = 0M, Decimal radius = 0M)
+		
+		simul.Start();
 		
 		return;
 	}
